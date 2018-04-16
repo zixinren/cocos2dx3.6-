93a3e01d9bbf4ba35b587e4fcf5b8602b8aed072
zixinren <dl198915@sina.cn>
2018-04-16 16:37:36 +0800

    first blood

---
+++ cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+E:\workspace\PROJECT\DouPoOnline\Client\trunk\frameworks\cocos2d-x\cocos\platform\android\java\bin\AndroidManifest.xml : \
+E:\workspace\PROJECT\DouPoOnline\Client\trunk\frameworks\cocos2d-x\cocos\platform\android\java\AndroidManifest.xml \
---
+++ cocos/platform/android/java/bin/classes/R.java.d
@@ -0,0 +1,4 @@
+E:\workspace\PROJECT\DouPoOnline\Client\trunk\frameworks\cocos2d-x\cocos\platform\android\java\gen\org\cocos2dx\lib\R.java \
+ : E:\workspace\PROJECT\DouPoOnline\Client\trunk\frameworks\cocos2d-x\cocos\platform\android\java\res\layout\screen_fit.xml \
+E:\workspace\PROJECT\DouPoOnline\Client\trunk\frameworks\cocos2d-x\cocos\platform\android\java\res\layout\screen_fit_h.xml \
+E:\workspace\PROJECT\DouPoOnline\Client\trunk\frameworks\cocos2d-x\cocos\platform\android\java\bin\AndroidManifest.xml \
